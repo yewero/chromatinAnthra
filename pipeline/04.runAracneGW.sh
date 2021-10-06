2a5b64a6f122a088e1fd1c8e007708c12641c7ab
yewero <tianf@mail.cbi.pku.edu.cn>
2021-10-06 12:49:35 +0800

    set the path for Aracne.jar

--- pipeline/04.runAracneGW.sh
+++ pipeline/04.runAracneGW.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
+
+jar_dir="/home/tianf/lustre/Tools/ARACNe-AP/dist"
+
 # threshold
-java -Xmx5G -jar Aracne.jar -e ../data/aracne_APtumor.exp  -o ../data/ARACNE_all_res --tfs ../data/aracne_APgeneList.exp --pvalue 1E-8 --seed 1 --calculateThreshold --threads 20
+java -Xmx5G -jar $jar_dir/Aracne.jar -e ../data/aracne_APtumor.exp  -o ../data/ARACNE_all_res --tfs ../data/aracne_APgeneList.exp --pvalue 1E-8 --seed 1 --calculateThreshold --threads 20
 
 # bootstrap
 for i in {1..100}
 do
 echo $i
-java -Xmx25G -jar Aracne.jar  -e ../data/aracne_APtumor.exp -o ../data/ARACNE_all_res --tfs ../data/aracne_APgeneList.exp --pvalue 1E-8 --threads 20 --seed $i
+java -Xmx25G -jar $jar_dir/Aracne.jar  -e ../data/aracne_APtumor.exp -o ../data/ARACNE_all_res --tfs ../data/aracne_APgeneList.exp --pvalue 1E-8 --threads 20 --seed $i
 done
 # consolidate
-java -Xmx25G -jar  Aracne.jar -o ../data/ARACNE_all_res/ --consolidate
+java -Xmx25G -jar $jar_dir/Aracne.jar -o ../data/ARACNE_all_res/ --consolidate
