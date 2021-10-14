cf52bbd406ab914eb4197ba0ced2fa41757fab99
yewero <tianf@mail.cbi.pku.edu.cn>
2021-10-14 19:42:46 +0800

    increase memory for Aracne

--- pipeline/04.runAracneGW.sh
+++ pipeline/04.runAracneGW.sh
@@ -9,7 +9,7 @@ java -Xmx5G -jar $jar_dir/Aracne.jar -e ../data/aracne_APtumor.exp  -o ../data/A
 for i in {1..100}
 do
 echo $i
-java -Xmx25G -jar $jar_dir/Aracne.jar  -e ../data/aracne_APtumor.exp -o ../data/ARACNE_all_res --tfs ../data/aracne_APgeneList.exp --pvalue 1E-8 --threads 20 --seed $i
+java -Xmx150G -jar $jar_dir/Aracne.jar  -e ../data/aracne_APtumor.exp -o ../data/ARACNE_all_res --tfs ../data/aracne_APgeneList.exp --pvalue 1E-8 --threads 20 --seed $i
 done
 # consolidate
 java -Xmx25G -jar $jar_dir/Aracne.jar -o ../data/ARACNE_all_res/ --consolidate
