5f73869bbce19f775145235eb60bd3c4ed6ca775
yewero <tianf@mail.cbi.pku.edu.cn>
2021-11-08 15:46:32 +0800

    modify 13

--- pipeline/13.anthra_vs_tax.R
+++ pipeline/13.anthra_vs_tax.R
@@ -123,7 +123,7 @@ load("../data/clinical/rmas/GSE65194/rmaData.RData")
 MAIRE = eset_rma
 MAIRE = MAIRE[,rownames(pData(MAIRE))]
 
-pData(MAIRE)$her2 = plyr::revalue(pData(MAIRE)$her2,c("no"="FALSE","yes"="TRUE"))
+#pData(MAIRE)$her2 = plyr::revalue(pData(MAIRE)$her2,c("no"="FALSE","yes"="TRUE"))
 
 table(pData(MAIRE)$chemo)
 levels(pData(MAIRE)$chemo)
@@ -145,7 +145,7 @@ mergedES = mergeNONE(list(KAO,INR,STK,UPS,MAIRE))
 
 pheno = pData(mergedES)
 library(plyr)
-pheno2 = colwise(function(x) plyr::revalue(x,c(" TRUE"="TRUE")))(pheno)
+pheno2 = colwise(function(x) { if(" TRUE" %in% levels(x)) { x <- plyr::revalue(x,c(" TRUE"="TRUE")) }; return(x) })(pheno)
 rownames(pheno2) = rownames(pheno)
 
 pheno2$clinicSubtype = factor(ifelse(pheno2$her2=="TRUE","Her2+",
@@ -172,7 +172,7 @@ danot$probe = rownames(danot)
 danot$Gene.Symbol =danot$`Gene Symbol`
 
 pheno = pData(mergedCOMBAT)
-pheno2 = colwise(function(x) plyr::revalue(x,c(" TRUE"="TRUE")))(pheno)
+pheno2 = colwise(function(x) { if(" TRUE" %in% levels(x)) { x <- plyr::revalue(x,c(" TRUE"="TRUE")) }; return(x) })(pheno)
 rownames(pheno2) = rownames(pheno)
 
 pheno2$clinicSubtype = factor(ifelse(pheno2$her2=="TRUE","Her2+",
@@ -181,6 +181,7 @@ pheno2$clinicSubtype = factor(ifelse(pheno2$her2=="TRUE","Her2+",
 
 pheno=pheno2
 pheno$age = as.numeric(as.character(pheno$age))
+pheno$grade <- as.numeric(as.character(pheno$grade))
 pheno$lymphNodeNum = as.numeric(as.character(pheno$lymphNodeNum))
 pheno$os.t = as.numeric(as.character(pheno$os.t))
 pheno$rdfs.t = as.numeric(as.character(pheno$rdfs.t))
@@ -188,7 +189,8 @@ pheno$rfs.t = as.numeric(as.character(pheno$rfs.t))
 pheno$size = as.numeric(as.character(pheno$size))
 pheno$t_stage = factor(as.numeric(as.character(pheno$t_stage)))
 
-
+# check blank
+! unique(sapply(1:ncol(pheno), function(i) { y <- any(grepl("^ ", pheno[, i])) }))
 
 pheno_other = data.frame(rfs.t=pheno$os.t/365,rfs.e=pheno$os.e=="TRUE",chemo = pheno$chemType,age=pheno$age,
                          er = as.factor(pheno$er),pr = as.factor(pheno$pr),her2 = as.factor(pheno$her2),
@@ -204,6 +206,7 @@ pheno_other_forTables$rfs.e =as.factor(pheno_other_forTables$rfs.e)
 exp_raw = exprs(mergedCOMBAT)
 
 pheno.df = na.omit(pheno_other)
+dim(pheno.df)
 
 pheno.df$cohort = droplevels(pheno.df$cohort)
 pheno.df$chemo = droplevels(pheno.df$chemo)
